38e103b0c4d794e94a56781a26f4f7db703efae9
Abhijeet Swami <swami2005abhijeet@gmail.com>
2025-06-01 21:24:10 +0530

    feat: Add basic array theory in JS

---
+++ Lec_1/arrayTheory.js
@@ -0,0 +1,28 @@
+// Arrays in JavaScript
+// An Array in JavaScript is a single variable that is used to store different elements. It's a fundamental data structure that allows you to store an ordered collection of values.
+
+// Key Characteristics of Arrays:
+// Ordered Collection: Elements are stored in a specific order and can be accessed by their numerical index (starting from 0).
+
+// Mutable: You can change, add, or remove elements after an array has been created.
+
+// Dynamic Size: Arrays can grow or shrink in size as needed.
+
+// Heterogeneous: An array can store elements of different data types (e.g., numbers, strings, booleans, objects).
+
+// Most Used Array Methods:
+// 1. push()
+// 2. pop()
+// 3. shift()
+// 4. unshift()
+// 5. splice()
+// 6. slice()
+// 7. concat()
+// 8. indexOf()
+// 9. includes()
+// 10. forEach() // loop
+// 11. map() // loop
+// 12. filter() // loop
+// 13. reduce()
+// 14. sort()
+// 15. reverse()
