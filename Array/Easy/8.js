34e93353bcebde42240262dbcb61605d37249f76
Abhijeet Swami <abhijeets.swami@gmail.com>
2025-12-29 15:53:01 +0530

    find missing number

---
+++ Array/Easy/8.js
@@ -0,0 +1,15 @@
+// Find missing number
+
+function missingNumber(nums) {
+  const n = nums.length;
+  let expected = (n * (n + 1)) / 2;
+  let actual = 0;
+
+  for (let num of nums) {
+    actual += num;
+  }
+
+  return expected - actual;
+}
+
+console.log(missingNumber([3, 0, 1]));
