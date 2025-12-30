311ba884b8e5b7515e6f78878de842e5d0b54691
Abhijeet Swami <abhijeets.swami@gmail.com>
2025-12-30 17:36:45 +0530

    single number

---
+++ Array/Easy/10.js
@@ -0,0 +1,13 @@
+// Single Number
+
+function singleNumber(nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+
+  return result;
+}
+
+console.log(singleNumber([1, 2, 1, 2, 4]));
