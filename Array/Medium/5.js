c8332155d916c6668453cf983fc05165c2cb3b37
Abhijeet Swami <abhijeets.swami@gmail.com>
2026-01-01 18:33:38 +0530

    stock buy or sell

---
+++ Array/Medium/5.js
@@ -0,0 +1,13 @@
+// Stock buy or sell
+
+function maxProfit(prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    minPrice = Math.min(minPrice, prices[i]);
+  }
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
